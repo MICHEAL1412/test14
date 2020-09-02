a6da5f1c4a9828284f0039389fc316cdd7e90fbd
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-09-02 09:44:07 +0800

    commit by ng

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.test1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ViewFlipper;
 
@@ -15,14 +15,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    LinearLayout Popular2,Coming_soon2,Now_showing2;
-
-    private ViewFlipper viewFlipper;
+    LinearLayout Popular,Coming_soon,Now_showing;
+    private Handler sliderHandler = new Handler();
+    private ViewPager2 viewPager2;
     private DrawerLayout drawer;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     static final String STATE_FRAGMENT = "state_of_fragment";
@@ -47,42 +54,64 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PolicyFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_policy);
         }
-        int images[]={R.drawable.baba_yaga___terror_of_the_dark_forest,R.drawable.digimon_adventure___last_evolution_kizuna,R.drawable.candyman_2020,
-                R.drawable.hitman___agent_jun,R.drawable.jumanji2,R.drawable.onward_};
-        viewFlipper=findViewById(R.id.v_flipper);
-
-        Popular2 = (LinearLayout)findViewById(R.id.Popular2);
-        Coming_soon2 = (LinearLayout)findViewById(R.id.Coming_soon2);
-        Now_showing2= (LinearLayout)findViewById(R.id.Now_showing2);
-
-        ImageView imageView=new ImageView(this);
 
 
-        viewFlipper.addView(imageView);
-        viewFlipper.setFlipInterval(3000);
-        viewFlipper.setAutoStart(true);
+        Popular = (LinearLayout)findViewById(R.id.textView);
+        Coming_soon = (LinearLayout)findViewById(R.id.textView2);
+        Now_showing= (LinearLayout)findViewById(R.id.textView3);
 
-        viewFlipper.setInAnimation(this,android.R.anim.slide_in_left);
-        viewFlipper.setInAnimation(this,android.R.anim.slide_out_right);
+        viewPager2=findViewById(R.id.viewPagerImageSlider);
+        List<SliderItem> sliderItems=new ArrayList<>();
+        sliderItems.add(new SliderItem(R.drawable.digimon_adventure___last_evolution_kizuna));
+        sliderItems.add(new SliderItem(R.drawable.my_hero_academia___heros_rising));
+        sliderItems.add(new SliderItem(R.drawable.hitman___agent_jun));
+        sliderItems.add(new SliderItem(R.drawable.baba_yaga___terror_of_the_dark_forest));
+        sliderItems.add(new SliderItem(R.drawable.jumanji2));
+        viewPager2.setAdapter(new SliderAdapter(sliderItems,viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r=1-Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable,3000);
+            }
+        });
 
 
     }
 
 
     public void Pop(View view){
-        Popular2.setVisibility(View.VISIBLE);
-        Coming_soon2.setVisibility(View.GONE);
-        Now_showing2.setVisibility(View.GONE);
+        Popular.setVisibility(View.VISIBLE);
+        Coming_soon.setVisibility(View.GONE);
+        Now_showing.setVisibility(View.GONE);
     }
     public void coming(View view){
-        Popular2.setVisibility(View.GONE);
-        Coming_soon2 .setVisibility(View.VISIBLE);
-        Now_showing2.setVisibility(View.GONE);
+        Popular.setVisibility(View.GONE);
+        Coming_soon .setVisibility(View.VISIBLE);
+        Now_showing.setVisibility(View.GONE);
     }
     public void Now(View view){
-        Popular2.setVisibility(View.GONE);
-        Coming_soon2.setVisibility(View.GONE);
-        Now_showing2.setVisibility(View.VISIBLE);
+        Popular.setVisibility(View.GONE);
+        Coming_soon.setVisibility(View.GONE);
+        Now_showing.setVisibility(View.VISIBLE);
     }
 
     public void Rating(View view) {
@@ -135,6 +164,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
         return false;
+    }
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+        }
+    };
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable,3000);
     }
 
 
